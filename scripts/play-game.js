b8186c6116aa0ab1f1adeb936a29591861f07e9e
ChauncySun1 <scy1013079677@gmail.com>
2026-03-19 15:27:31 +0800

    improve console output: specific species name

--- scripts/play-game.js
+++ scripts/play-game.js
@@ -3,10 +3,15 @@ const {ethers} = require("hardhat");
 async function main() {
     const [deployer, player] = await ethers.getSigners();
 
-    const TOKEN_ADDRESS = "0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E";
-    const NFT_ADDRESS = "0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690";
-    const POND_ADDRESS = "0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB";
+    const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const NFT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+    const POND_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
+
+    const FISH_NAMES = ["Crucian Carp", "Gibel Carp", "Common Carp", "F1", "Mirror Carp",
+        "Orenji Ogon", "Kohaku", "Hi Utsuri", "Mameshibori Goshiki", "Yotsushiro",
+        "Perch", "Pike", "Catfish", "Albino Catfish"
+    ];
 
     const token = await ethers.getContractAt("ChauncyFishingToken", TOKEN_ADDRESS);
     const nft = await ethers.getContractAt("ChauncyFishNFT", NFT_ADDRESS);
@@ -55,7 +60,9 @@ async function main() {
 
     if (event){
         const decoded = pond.interface.parseLog(event);
-        console.log(`Successfully caught a ${decoded.args.dietGroup} fish, speices id: ${decoded.args.typeId}. TokenID: ${decoded.args.tokenId}`);
+
+        const typeName = FISH_NAMES[decoded.args.typeId];
+        console.log(`Successfully caught a ${decoded.args.dietGroup} fish, speices: ${typeName}. TokenID: ${decoded.args.tokenId}`);
 
     }
 
